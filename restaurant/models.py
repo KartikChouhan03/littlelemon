bbe62de08d33ef56ef710aaad9c9d2d0c920cae0
KartikChouhan03 <chouhankartik3300@gmail.com>
2025-06-22 16:40:37 +0530

    Added unit tests and tested menu api with insomnia

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,8 +6,8 @@ class Menu(models.Model):
     price = models.DecimalField(max_digits=10,decimal_places= 2)
     inventory = models.IntegerField()
     
-    def __self__(self):
-        return self.title
+    def __str__(self):
+        return f'{self.title} : {self.price}'
     
 class Booking(models.Model):
     name = models.CharField(max_length=255)
